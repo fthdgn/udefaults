ca4397f03aa01ebb2aa75ff334d45e61c0b7e03d
Fatih Doğan <fatih@fatihdogan.dev>
2020-05-12 21:58:43 +0300

    wip

--- Sources/UDefaults/UDefaults.swift
+++ Sources/UDefaults/UDefaults.swift
@@ -1,3 +1,123 @@
-struct UDefaults {
-    var text = "Hello, World!"
+import Foundation
+
+protocol UDefaults {
+    var userDefaults: UserDefaults { get }
+}
+
+class UMapKey<T: Codable> {
+    private unowned let userDefaults: UserDefaults
+    private let key: String
+    private let defaultValue: T
+    private let decoder: JSONDecoder
+    private let encoder: JSONEncoder
+    
+    init(_ uDefaults: UDefaults, _ key: String, defValue: T) {
+        self.userDefaults = uDefaults.userDefaults
+        self.key = key
+        self.decoder = .init()
+        self.encoder = .init()
+        self.defaultValue = defValue
+    }
+    
+    private func doesUserDefaultSupport() -> Bool {
+        return T.self is NSNumber.Type ||
+            T.self is NSString.Type ||
+            T.self is NSDate.Type ||
+            T.self is String.Type ||
+            T.self is Int.Type ||
+            T.self is Int8.Type ||
+            T.self is Int16.Type ||
+            T.self is Int32.Type ||
+            T.self is Int64.Type ||
+            T.self is UInt8.Type ||
+            T.self is UInt16.Type ||
+            T.self is UInt32.Type ||
+            T.self is UInt64.Type ||
+            T.self is Float.Type ||
+            T.self is Double.Type ||
+            T.self is Date.Type ||
+            T.self is Data.Type
+    }
+    
+    
+    subscript(indexKey: String) -> T {
+        get{
+            let currentKey = "\(key)[\(indexKey)]"
+            if (doesUserDefaultSupport()) {
+                return (userDefaults.value(forKey: currentKey) as? T) ?? defaultValue
+            }
+            
+            if let data = userDefaults.value(forKey: currentKey) as? Data {
+                return try! decoder.decode(T.self, from: data)
+            }
+            return defaultValue
+        }
+        
+        set(newValue) {
+            let currentKey = "\(key)[\(indexKey)]"
+            if (doesUserDefaultSupport()) {
+                userDefaults.setValue(newValue, forKey: currentKey)
+            } else {
+                userDefaults.setValue(try! encoder.encode(newValue), forKey: currentKey)
+            }
+        }
+    }
+}
+
+class UKey<T:Codable> {
+    
+    private unowned let userDefaults: UserDefaults
+    private let key: String
+    private let defaultValue: T
+    private let decoder: JSONDecoder
+    private let encoder: JSONEncoder
+    
+    init(_ uDefaults: UDefaults, _ key: String, defValue: T) {
+        self.userDefaults = uDefaults.userDefaults
+        self.key = key
+        self.decoder = .init()
+        self.encoder = .init()
+        self.defaultValue = defValue
+    }
+    
+    private func doesUserDefaultSupport() -> Bool {
+        return T.self is NSNumber.Type ||
+            T.self is NSString.Type ||
+            T.self is NSDate.Type ||
+            T.self is String.Type ||
+            T.self is Int.Type ||
+            T.self is Int8.Type ||
+            T.self is Int16.Type ||
+            T.self is Int32.Type ||
+            T.self is Int64.Type ||
+            T.self is UInt8.Type ||
+            T.self is UInt16.Type ||
+            T.self is UInt32.Type ||
+            T.self is UInt64.Type ||
+            T.self is Float.Type ||
+            T.self is Double.Type ||
+            T.self is Date.Type ||
+            T.self is Data.Type
+    }
+    
+    var value: T {
+        get{
+            if (doesUserDefaultSupport()) {
+                return (userDefaults.value(forKey: key) as? T) ?? defaultValue
+            }
+            
+            if let data = userDefaults.value(forKey: key) as? Data {
+                return try! decoder.decode(T.self, from: data)
+            }
+            return defaultValue
+        }
+        
+        set(newValue) {
+            if (doesUserDefaultSupport()) {
+                userDefaults.setValue(newValue, forKey: key)
+            } else {
+                userDefaults.setValue(try! encoder.encode(newValue), forKey: key)
+            }
+        }
+    }
 }
--- Tests/UDefaultsTests/UDefaultsTests.swift
+++ Tests/UDefaultsTests/UDefaultsTests.swift
@@ -2,14 +2,33 @@ import XCTest
 @testable import UDefaults
 
 final class UDefaultsTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(UDefaults().text, "Hello, World!")
+    func testString() {
+        UserDefaults.standard.removePersistentDomain(forName: "testString")
+        class MyDefaults: UDefaults {
+            var userDefaults: UserDefaults = UserDefaults.init(suiteName: "testString")!
+            private(set) lazy var string = UKey<String>(self, "string", defValue: "def")
+        }
+        let myDefaults = MyDefaults()
+        XCTAssertEqual(myDefaults.string.value, "def")
+        myDefaults.string.value = "qwerty"
+        XCTAssertEqual(myDefaults.string.value, "qwerty")
     }
-
+    
+    func testStringMap() {
+        UserDefaults.standard.removePersistentDomain(forName: "testStringMap")
+        class MyDefaults: UDefaults {
+            var userDefaults: UserDefaults = UserDefaults.init(suiteName: "testStringMap")!
+            private(set) lazy var string = UMapKey<String>(self, "string", defValue: "def")
+        }
+        
+        let myDefaults = MyDefaults()
+        XCTAssertEqual(myDefaults.string["a"], "def")
+        myDefaults.string["a"] = "qwerty"
+        XCTAssertEqual(myDefaults.string["a"], "qwerty")
+    }
+    
     static var allTests = [
-        ("testExample", testExample),
+        ("testString", testString),
+        ("testStringMap", testStringMap),
     ]
 }
